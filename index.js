feab67073cfd95cda2eaee44e8c8c809c95800f1
DeveloperKaue <kauebarretobenicio@gmail.com>
2025-08-05 04:32:05 -0300

    Modificações na array de usuários fakes
    Algumas alterações com + 10 novos usuários fakes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const emailRoutes = require('./src/routes/email');
 //Rotas de usuários fakes
 API.use('/', fakeUserRoutes);
 
-//Rotas de validação de email
+//Rotas de validação e geração de emails
 API.use('/', emailRoutes);
 
 API.listen(8080, () => {
